7867c687b7531cd4738dd858c2090e87bd20fc64
toshi <sakao@kth.se>
2023-03-31 13:11:01 +0200

    Made project
    issue #20
    - added gitignore
    - added some files and folders
    - added some boilerplate code for piston.

---
+++ Physics2D/src/game.rs
@@ -0,0 +1,25 @@
+mod update;
+mod draw;
+pub struct Game {
+
+}
+
+impl Game {
+    pub fn new() -> Game{
+        Game {
+            
+        }
+    }
+
+    pub fn init() {
+
+    }
+
+    pub fn update() {
+
+    }
+
+    pub fn draw() {
+
+    }
+}
---
+++ Physics2D/src/main.rs
@@ -0,0 +1,36 @@
+// use opengl_graphics::OpenGL;
+use piston::{
+    input::{RenderEvent, UpdateEvent},
+    window::WindowSettings,
+};
+// use piston_window::{Event, PistonWindow};
+
+mod game;
+
+fn main() {
+    use std::{thread, time};
+
+    let mut window =
+        WindowSettings::new("Chapter 2 Forces", (SCREEN_WIDTH, SCREEN_HEIGHT))
+            .exit_on_esc(true)
+            // .graphics_api(OpenGL::V3_2)
+            .build()
+            .unwrap();
+
+    let mut game = game::Game::new();
+
+    game.init();
+
+    // game loop
+    while let Some(event) = window.next() {
+        if let Some(_) = event.render_args() {
+            app.render(&event, &mut window);
+        }
+        if let Some(_) = event.update_args() {
+            app.update();
+        }
+
+        let duration = time::Duration::from_millis(10);
+        thread::sleep(duration);
+    }
+}
