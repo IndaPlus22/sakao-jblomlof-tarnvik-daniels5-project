1113e29ab939d553afa524f566a565df76a0a825
tarnvik <Tilde@tarnvik.com>
2023-03-31 14:35:45 +0200

    Make it cerise
    #20
    
    You know I had to do it

--- Physics2D/src/game/draw.rs
+++ Physics2D/src/game/draw.rs
@@ -5,11 +5,13 @@ use piston::{
 };
 use piston_window::{Event, PistonWindow};
 
+const CERISE_COLOR: [f32; 4] = [232.0/255.0, 61.0/255.0, 132.0/255.0, 1.0];
+
 pub fn draw(event: &Event, window: &mut PistonWindow) {
 
     // Update application window.
     window.draw_2d(event, |context, graphics, _| {
         // Fill the window with white colour.
-        piston_window::clear([0.0; 4], graphics);
+        piston_window::clear(CERISE_COLOR, graphics);
     });
 }
