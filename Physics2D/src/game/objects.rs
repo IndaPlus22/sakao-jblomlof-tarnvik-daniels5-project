3ce71f1b2f7fc68231576076ae60ec91196daf79
danielsv03 <danielsvensson2003@gmail.com>
2023-04-03 13:40:21 +0200

    Add circle & update object trait

--- Physics2D/src/game/objects.rs
+++ Physics2D/src/game/objects.rs
@@ -11,9 +11,21 @@ pub struct Rectangle{
     potnrg: f64,
 }
 
+pub struct Circle{
+    center: Vec2d,
+    radius: isize,
+    mass: usize,
+    velocity: f64,
+    potnrg: f64,
+}
+
 impl Object for Rectangle {
     fn collisions(&self, other: &Box<dyn Object>, record: Option<collisionRecord>) -> Option<super::traits::collisionRecord> {
-        
+        //Vi kommer behöva göra en if statement eller en case switch som kollar följande
+        //Om other är en circel
+        //Om other är en rectanglel
+        //Osv
+        //Då jag inte lyckas hitta någon generell formel för alla möjliga former.
         return record;
     }
     fn update(&self) {
@@ -21,5 +33,23 @@ impl Object for Rectangle {
     }
     fn draw(&self) {
         
+    }
+    fn getcenter(&self) -> Vec2d {
+        return self.center;
+    }
+}
+
+impl Object for Circle {
+    fn collisions(&self, other: &Box<dyn Object>, record: Option<collisionRecord>) -> Option<collisionRecord> {
+        return record;
+    }
+    fn update(&self) {
+        
+    }
+    fn draw(&self) {
+        
+    }
+    fn getcenter(&self) -> Vec2d {
+        return self.center
     }
 }
--- Physics2D/src/game/traits.rs
+++ Physics2D/src/game/traits.rs
@@ -1,7 +1,10 @@
+use graphics::types::Vec2d;
+
 pub trait Object {
     fn collisions(&self, other: &Box<dyn Object>, record: Option<collisionRecord>) -> Option<collisionRecord>;
     fn update(&self);
     fn draw(&self);
+    fn getcenter(&self) -> Vec2d;
 }
 
 pub struct collisionRecord {
