8b72d0664f8f49c5d733b1de9fd64ebb58b2d13e
toshi <sakao@kth.se>
2023-04-03 12:33:50 +0200

    more code in buttons
    #22
    - added more functionality in buttons

--- Physics2D/src/game/draw.rs
+++ Physics2D/src/game/draw.rs
@@ -1,5 +1,6 @@
 mod button;
 
+use graphics::{types::Vec2d, color};
 use opengl_graphics::OpenGL;
 use piston::{
     input::{RenderEvent, UpdateEvent},
@@ -10,6 +11,7 @@ use piston_window::{Event, PistonWindow};
 use button::Button;
 
 const CERISE_COLOR: [f32; 4] = [232.0/255.0, 61.0/255.0, 132.0/255.0, 1.0];
+const LIGHT_CERISE_COLOR: &str = "ec5f99";
 
 pub fn draw(event: &Event, window: &mut PistonWindow) {
     // Update application window.
@@ -18,14 +20,18 @@ pub fn draw(event: &Event, window: &mut PistonWindow) {
         piston_window::clear(CERISE_COLOR, graphics);
 
         // TODO: put all graphics shit here
-        play_bar();
+        play_bar(Vec2d::from([100.0, 100.0]));
     });
 }
 
 // TODO: 
-fn play_bar() {
-    let mut play_button = Button::new();
-    let mut restart_button = Button::new();
+fn play_bar(pos: Vec2d) { // pos is upper-left corner
+    let size: Vec2d = Vec2d::from([30.0, 30.0]);
+    let play_pos: Vec2d = Vec2d::from([pos[0] + 10.0, pos[1] - 10.0]);
+    let restart_pos: Vec2d = Vec2d::from([pos[0] + size[0] + 10.0, pos[1] - size[1] - 10.0]);
+
+    let mut play_button = Button::new(play_pos, size, color::hex(LIGHT_CERISE_COLOR));
+    let mut restart_button = Button::new(restart_pos, size, color::hex(LIGHT_CERISE_COLOR));
 }
 
 // TODO: 
--- Physics2D/src/game/draw/button.rs
+++ Physics2D/src/game/draw/button.rs
@@ -1,16 +1,34 @@
+use graphics::{types::Vec2d, color};
+use opengl_graphics::OpenGL;
+use piston::{
+    input::{RenderEvent, UpdateEvent},
+    window::WindowSettings,
+};
+use piston_window::{Event, PistonWindow, types::Color, Rectangle, rectangle::square};
+
 // struct for button in UI.
 // TODO: put in parameters such as:
 // position, size, funtionality, (and other things that don't come up to mind right now)
 pub struct Button {
-
+    dims: [f64; 4],
+    // action: ,
+    // what todo,
+    color: Color,
+    rect: Rectangle
 }
 
 impl Button {
     // constructor for Button
-    pub fn new() -> Button {
+    pub fn new(pos: Vec2d, size: Vec2d, color: Color) -> Button {
+        let rect = Rectangle::new(color);
+        let dims = square(pos[0], pos[1], size[0]);
         Button {
-
+            dims, color, rect
         }
+    }
+
+    pub fn draw(&self) {
+        // rect.draw(dims, &draw_state::Default::default(), transform, g);
     }
 
     // TODO: Add features such as the functionality
