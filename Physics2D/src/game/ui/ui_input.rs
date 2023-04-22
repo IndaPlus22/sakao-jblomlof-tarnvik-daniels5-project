147b9b6422854ea934dafb53a991211fff0d9fbe
tarnvik <Tilde@tarnvik.com>
2023-04-22 15:18:54 +0200

    add all the buttons with check hover
    #56 The basic structure of all the buttons is complete, the next steps are creating and loading graphics as well as adding the actual functionality of the buttons

--- Physics2D/src/game/ui/ui_input.rs
+++ Physics2D/src/game/ui/ui_input.rs
@@ -7,7 +7,7 @@ use super::ui_objects::Objects;
 pub fn input(event: &Event, objects: &mut Objects, variables: &mut Variables){
 
     if let Some(pos) = event.mouse_cursor_args() {
-        for i in 0..2 {
+        for i in 0..5 {
             objects.buttons[i].check_hover(pos);
             println!("Button {}: hover={}", i, objects.buttons[i].hover);
         }
@@ -17,6 +17,13 @@ pub fn input(event: &Event, objects: &mut Objects, variables: &mut Variables){
             variables.game_state = GameState::Running;
         } else if objects.buttons[1].hover{
             variables.game_state  = GameState::Paused;
+        } else if objects.buttons[2].hover{
+            //TODO: Save button functionality aka save the current objects in a file
+        } else if objects.buttons[3].hover{
+            //TODO: Restart button functionality aka reset the simulation to the last saved state 
+            //TODO: If there is no saved state defualt is an empty file? 
+        } else if objects.buttons[4].hover{
+            //TODO: Clear button functionality aka delete all objects, making the thing blank
         } 
     }
     // if let Some(button) = event.release_args() {
